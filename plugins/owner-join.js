5738465c481606af83452ea641d793dae3c15f4c
Danielbot01 <159606732+Danielbot01@users.noreply.github.com>
2024-03-06 23:13:37 -0600

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
      enviando = true 
   try {
     const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
-    if (!link || !link.match(linkRegex)) throw '*[❗] Link erroneo o faltante, ingrese el enlace de un grupo de WhatsApp.*\n\n*—◉ Ejemplo:*\n*◉ .join https://chat.whatsapp.com/GuLhkVDo8XX3VeIXU93psI*';
+    if (!link || !link.match(linkRegex)) throw '*[❗] Link erroneo o faltante, ingrese el enlace de un grupo de WhatsApp.*\n\n*—◉ Ejemplo:*\n*◉ .join https://chat.whatsapp.com/KsczcPOIczT2REac09fn5n*';
     const [_, code] = link.match(linkRegex) || [];
     if ( isPrems || isMods || isOwner || m.fromMe) {
       const res = await conn.groupAcceptInvite(code);
